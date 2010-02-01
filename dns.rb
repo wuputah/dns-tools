17c43649389c49d4373da9c51729944ca65d9f43
Jonathan Dance <jd@wuputah.com>
2010-02-01 09:03:44 -0500

    ubiquitous @hostname, set default values to nil

--- dns.rb
+++ dns.rb
@@ -9,12 +9,16 @@ class BadInputError < StandardError; end
 
 helpers do
   def clean_hostname(hn)
-    hn.strip.sub(/[><|&;\s].*/, '')
+    hn = hn.strip.sub(/[><|&;\s].*/, '')
+    raise BadInputError if hn.length == 0
+    hn
   end
 end
 
 before do
   headers 'Cache-Control' => 'public, max-age=60'
+  @hostname = nil
+  @output = nil
 end
 
 get '/' do
@@ -25,34 +29,32 @@ end
 ['/dig/:type/:hostname', '/dig/:hostname'].each do |path|
   get path do
     type = %w[a mx ns any txt srv aaaa].include?(params[:type]) ? params[:type] : 'a'
-    hostname = clean_hostname(params[:hostname])
-    raise BadInputError if hostname.length == 0
-    @output = "<strong>$ dig #{type} #{hostname}</strong>\n" +
-              `#{Escape.shell_command(["dig", type, hostname])}`.strip
+    @hostname = clean_hostname(params[:hostname])
+    @output = "<strong>$ dig #{type} #{@hostname}</strong>\n" +
+              `#{Escape.shell_command(["dig", type, @hostname])}`.strip
     erb :index
   end
 end
 
 get '/reverse/:hostname' do
   raise BadInputError unless params[:hostname] =~ IP_REGEXP
-  @output = "<strong>$ dig -x #{params[:hostname]}</strong>\n" +
-            `#{Escape.shell_command(["dig", "-x", params[:hostname]])}`.strip
+  @hostname = params[:hostname]
+  @output = "<strong>$ dig -x #{@hostname}</strong>\n" +
+            `#{Escape.shell_command(["dig", "-x", @hostname])}`.strip
   erb :index
 end
 
 get '/lookup/:hostname' do
-  hostname = clean_hostname(params[:hostname])
-  raise BadInputError if hostname.length == 0
-  @output = "<strong>$ nslookup #{params[:hostname]}</strong>\n" +
-            `#{Escape.shell_command(["nslookup", params[:hostname]])}`.strip
+  @hostname = clean_hostname(params[:hostname])
+  @output = "<strong>$ nslookup #{@hostname}</strong>\n" +
+            `#{Escape.shell_command(["nslookup", @hostname])}`.strip
   erb :index
 end
 
 get '/whois/:hostname' do
-  hostname = clean_hostname(params[:hostname])
-  raise BadInputError if hostname.length == 0
+  @hostname = clean_hostname(params[:hostname])
   @output = "<strong>$ whois #{params[:hostname]}</strong>\n" +
-            `#{Escape.shell_command(["whois", params[:hostname]])}`.strip
+            `#{Escape.shell_command(["whois", @hostname])}`.strip
   erb :index
 end
 
